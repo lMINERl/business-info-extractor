9b55bc304bd5abfe9b527401b0d6cb6fa28dd0d3
lMINERl <seect19944@gmail.com>
2020-06-07 12:17:43 +0200

    add redux to project

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,19 @@ import './css/index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
+import { rootReducer } from './store';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+// import thunk from 'react-thunk';
+
+const myStore = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={myStore}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
