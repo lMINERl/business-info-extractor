4b6dade245a0d7c1f8be71b02cb8a597d2eb193c
lMINERl <seect19944@gmail.com>
2020-06-27 16:17:52 +0200

    testing reducers,redux,dispatch and storybook elements

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import logo from '../assets/logo.svg';
 import './App.css';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn Reacts
-      </a>
-    </header>
-  </div>
-);
+import { Button } from '@material-ui/core';
+
+import { InputTelephoneNumber, InputVariant, InputPassword, InputText } from '../stories/inputs';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../store';
+import { getAllData } from '../store/actions/DataActions';
+const App: React.FC = () => {
+  const [formState, setFromState] = useState<{ [key: string]: any }>({});
+  const changeHandle = (event: ChangeEvent<HTMLInputElement>) => setFromState({ ...formState, [event.target.id]: event.target.value });
+
+  const reducers = useSelector((state: RootState) => state.data);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    // dispatch(getAllData);
+  }, []);
+
+  const handleSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    // console.log(reducers.filteredList);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn Reacts
+        </a>
+      </header>
+      <div className="App">
+        {InputTelephoneNumber(InputVariant.standard, changeHandle, 'textMast1')}
+        {InputTelephoneNumber(InputVariant.standard, changeHandle, 'textMask2')}
+        {InputPassword(InputVariant.outlined, changeHandle, 'password')}
+        <br />
+        {InputText(InputVariant.standard, 'AnyText', false, true, changeHandle, 'textinput')}
+        <Button variant="outlined" onClick={handleSubmit}>
+          submit
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
