ebabdff64523a9e6aac21484a59afa204228f3e7
lMINERl <seect19944@gmail.com>
2020-08-04 17:32:15 +0200

    add breadcrumbs without routing but given the name of content

--- src/stories/breadcrumbs/breadcrumbs.stories.tsx
+++ src/stories/breadcrumbs/breadcrumbs.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { BreadcrumbsDefault } from '.';
+import { BreadcrumbsDefault, BreadcrumbsNoRoute } from '.';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { CheckboxDefault } from '../checkbox';
@@ -41,5 +41,38 @@ storiesOf('Breadcrumbs', module)
           <Route path="/" exact component={() => <CheckboxDefault content={{ keyId: 'h', label: 'Home' }} handleChange={() => {}} />} />
         </Switch>
       </BrowserRouter>
+    );
+  })
+  .add('BreadcrumbsNoRoute', () => {
+    const [breadcrumbsState, dispatchCurrentLocation] = React.useReducer(
+      (state: { prevLoc: { key: string }[]; currLoc: { key: string } }, action: { key: string }) => {
+        let newState = { ...state };
+        let newPrevLoc = [...newState.prevLoc];
+        let newCurrLoc = { key: action.key };
+        if (state.currLoc.key === action.key) {
+          return { ...state, prevLoc: newPrevLoc, currLoc: newCurrLoc };
+        }
+        if (newPrevLoc.some((v) => v.key === action.key)) {
+          newPrevLoc = newPrevLoc.slice(
+            0,
+            newPrevLoc.findIndex((loc) => loc.key === action.key)
+          );
+        } else {
+          newPrevLoc.push(state.currLoc);
+        }
+        return { ...state, prevLoc: newPrevLoc, currLoc: newCurrLoc };
+      },
+      {
+        prevLoc: [{ key: 'Home' }, { key: 'Contacts' }],
+        currLoc: { key: 'Ahmed' }
+      }
+    );
+    return (
+      <BreadcrumbsNoRoute
+        dispatchCurrentLocation={dispatchCurrentLocation}
+        previousLocations={breadcrumbsState.prevLoc}
+        currentLocation={breadcrumbsState.currLoc}
+        options={{ maxItems: 5 }}
+      />
     );
   });
--- src/stories/breadcrumbs/index.tsx
+++ src/stories/breadcrumbs/index.tsx
@@ -61,3 +61,55 @@ export const BreadcrumbsDefault = (props: {
     </Breadcrumbs>
   );
 };
+
+export const BreadcrumbsNoRoute = (props: {
+  options?: { maxItems: number };
+  dispatchCurrentLocation: (action: { key: string; index: number }) => void;
+  previousLocations?: { key: string }[];
+  currentLocation?: { key: string };
+}) => {
+  const options = props.options ?? { maxItems: 5 };
+  const prevLocations = props.previousLocations ?? [];
+  const currLocation = props.currentLocation ?? { key: 'Home' };
+
+  const prevLocLinks = useMemo(
+    () =>
+      prevLocations.map(({ key }, index: number) => {
+        return (
+          <Link
+            key={index}
+            color="inherit"
+            href="#"
+            onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+              event.preventDefault();
+              props.dispatchCurrentLocation({ key: key, index: index });
+            }}
+          >
+            {key}
+          </Link>
+        );
+      }),
+    [props, prevLocations]
+  );
+  let currLocLink = useMemo(
+    () => (
+      <Link
+        color="textPrimary"
+        href="#"
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+          event.preventDefault();
+        }}
+      >
+        {currLocation.key}
+      </Link>
+    ),
+    [currLocation]
+  );
+
+  return (
+    <Breadcrumbs maxItems={options.maxItems} aria-label="breadcrumb">
+      {prevLocLinks}
+      {currLocLink}
+    </Breadcrumbs>
+  );
+};
