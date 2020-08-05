b6cfa765e02148a3ce46712c04a5d840e0fb1e31
lMINERl <seect19944@gmail.com>
2020-08-05 03:11:04 +0200

    add routeDefault to add routes and switch components without  using react-route-dom

---
+++ src/stories/route/index.tsx
@@ -0,0 +1,18 @@
+import React, { useMemo } from 'react';
+
+const Route = (props: { component: (props: any) => JSX.Element; props: { [key: string]: any } }) => {
+  const component = useMemo(() => <props.component {...props.props} />, [props.props]);
+  return component;
+};
+
+export const RouteDefault = (props: {
+  selectedKey: string;
+  components: { key: string; component: (props: any) => JSX.Element; props?: { [key: string]: any } }[];
+}) => {
+  const components = useMemo(() => props.components.map((c) => <Route key={c.key} component={c.component} props={c.props ?? {}} />), [
+    props.components
+  ]);
+  const component = components.filter((c) => c.key === props.selectedKey);
+
+  return <div>{component.length ? component[0] : null}</div>;
+};
---
+++ src/stories/route/route.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { RouteDefault } from '.';
+
+storiesOf('Route', module)
+  .addDecorator((story) => <ThemeProvider theme={createMuiTheme({ palette: { type: 'light' } })}>{story()}</ThemeProvider>)
+  .add('RouteDefault', () => {
+    const [selected, setSelected] = React.useState('A');
+    const A = (props: { a: string }) => <div>this is component{props.a}</div>;
+    const B = (props: { b: string }) => <div>this is component{props.b}</div>;
+    const C = (props: { c: string }) => <div>this is component{props.c}</div>;
+    return (
+      <div>
+        <div style={{ display: 'inline-flex' }}>
+          <button onClick={() => setSelected('A')}>Select A</button>
+          <button onClick={() => setSelected('B')}>Select B</button>
+          <button onClick={() => setSelected('C')}>Select C</button>
+        </div>
+        <RouteDefault
+          selectedKey={selected}
+          components={[
+            { key: 'A', component: A, props: { a: 'A' } },
+            { key: 'B', component: B, props: { b: 'B' } },
+            { key: 'C', component: C, props: { c: 'C' } }
+          ]}
+        />
+      </div>
+    );
+  });
