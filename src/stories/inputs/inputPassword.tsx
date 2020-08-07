1b2f7c91474198ff65107c5e78d4105c52fc4592
lMINERl <seect19944@gmail.com>
2020-08-07 06:43:26 +0200

    add error text if error and placeholder fix addAdorment to be memorized add dummy addAorement to input so all input components to be one size

--- src/stories/inputs/inputPassword.tsx
+++ src/stories/inputs/inputPassword.tsx
@@ -1,48 +1,65 @@
 import React, { useState, useMemo } from 'react';
 import LabelDefault from '../labels/labelDefault';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
-import { InputAdornment, IconButton } from '@material-ui/core';
+import { InputAdornment, IconButton, Typography } from '@material-ui/core';
 import { getInputType } from './inputText';
 
 const InputPassword = (props: {
   variant?: 'standard' | 'outlined' | 'fill';
   changeHandle: any;
+  isError?: boolean;
   content: {
     keyId: string;
     defaultValue?: string;
+    placeholder?: string;
+    errotText?: string;
   };
 }) => {
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
   // const label = useMemo(() => , [keyId]);
 
-  const handleClickShowPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setShowPassword(!showPassword);
-  };
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-  };
-
   let InputVariant = getInputType(props.variant);
-  const inputLabel = useMemo(() => <LabelDefault content={{ text: 'password', forId: props.content.keyId }} />, [props.content.keyId]);
+  const inputLabel = useMemo(() => <LabelDefault content={{ text: 'Password', forId: props.content.keyId }} />, [props.content.keyId]);
+  const inputAdornment = useMemo(
+    () => (
+      <InputAdornment position="end">
+        <IconButton
+          aria-label="toggle password visibility"
+          onClick={() => setShowPassword(!showPassword)}
+          onMouseDown={(e) => {
+            e.preventDefault();
+          }}
+        >
+          {showPassword ? <VisibilityOff /> : <Visibility />}
+        </IconButton>
+      </InputAdornment>
+    ),
+    [showPassword]
+  );
+  const errorText = useMemo(
+    () => (
+      <Typography variant="inherit" style={{ color: 'red' }}>
+        {props.isError ? props.content.errotText : ''}
+      </Typography>
+    ),
+    [props.isError, props.content.errotText]
+  );
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
       {inputLabel}
       <InputVariant
         type={showPassword ? 'text' : 'password'}
         onChange={(e) => props.changeHandle({ target: { name: props.content.keyId, id: props.content.keyId, value: e.target.value } } as any)}
         required={true}
+        error={props.isError}
         name={props.content.keyId}
         defaultValue={props.content.defaultValue}
         id={props.content.keyId}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword}>
-              {showPassword ? <VisibilityOff /> : <Visibility />}
-            </IconButton>
-          </InputAdornment>
-        }
+        placeholder={props.content.placeholder}
+        endAdornment={inputAdornment}
       />
+      {errorText}
     </div>
   );
 };
--- src/stories/inputs/inputText.tsx
+++ src/stories/inputs/inputText.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
-import { Input, FilledInput, OutlinedInput } from '@material-ui/core';
+import { Input, FilledInput, OutlinedInput, InputAdornment, IconButton, Typography } from '@material-ui/core';
 import LabelDefault from '../labels/labelDefault';
+import { VisibilityOff } from '@material-ui/icons';
 
 export const getInputType = (variant: 'standard' | 'outlined' | 'fill' | undefined) => {
   switch (variant) {
@@ -23,6 +24,8 @@ const InputText = (props: {
     labelText: string;
     keyId: string;
     defaultValue?: string;
+    placeholder?: string;
+    errorText?: string;
   };
 }) => {
   let InputVariant = getInputType(props.variant);
@@ -30,10 +33,29 @@ const InputText = (props: {
     props.content.keyId,
     props.content.labelText
   ]);
+  const inputAdorment = useMemo(
+    () => (
+      <InputAdornment position="end" style={{ width: '0%' }}>
+        <IconButton style={{ visibility: 'hidden' }} aria-label="toggle password visibility">
+          <VisibilityOff />
+        </IconButton>
+      </InputAdornment>
+    ),
+    []
+  );
+  const errorText = useMemo(
+    () => (
+      <Typography variant="inherit" style={{ color: 'red' }}>
+        {props.isError ? props.content.errorText : ''}
+      </Typography>
+    ),
+    [props.isError, props.content.errorText]
+  );
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
       {inputLabel}
       <InputVariant
+        className=""
         error={props.isError}
         required={props.isRequired}
         type="text"
@@ -41,8 +63,11 @@ const InputText = (props: {
         onChange={(e) => props.changeHandle({ target: { name: props.content.keyId, id: props.content.keyId, value: e.target.value } } as any)}
         name={props.content.keyId}
         id={props.content.keyId}
+        placeholder={props.content.placeholder}
         inputComponent={props.inputComponent as any}
+        endAdornment={inputAdorment}
       />
+      {errorText}
     </div>
   );
 };
