b6c1e72edb348db0b05ba92efe9597b272999ee6
lMINERl <seect19944@gmail.com>
2020-08-08 04:40:27 +0200

    no message

---
+++ storybook-static/precache-manifest.18f907f06f4107dbd2b6328e2ec3e4c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553b3acdc27d2986e76d14f4d0c425df",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.7a8038b48e4ba1fdfe28.bundle.js"
+  },
+  {
+    "url": "runtime~main.7a8038b48e4ba1fdfe28.bundle.js"
+  },
+  {
+    "revision": "71db0a999c8018d02e1b",
+    "url": "static/css/main.ebd89eff.chunk.css"
+  },
+  {
+    "url": "vendors~main.7a8038b48e4ba1fdfe28.bundle.js"
+  },
+  {
+    "url": "vendors~main.7a8038b48e4ba1fdfe28.bundle.js.LICENSE.txt"
+  }
+]);
