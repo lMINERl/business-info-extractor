139a55983347c42aba7069663e9fcf48d9c1c958
lMINERl <seect19944@gmail.com>
2020-08-15 10:42:41 +0200

    improve card skeleton layout

--- src/stories/cards/cardSkeleton.tsx
+++ src/stories/cards/cardSkeleton.tsx
@@ -5,8 +5,8 @@ import Grid from '@material-ui/core/Grid';
 
 const CardSkeleton = (props: {}) => {
   return (
-    <Paper style={{ maxWidth: 345, height: 248, maxHeight: 248 }} elevation={3}>
-      <div className="card-skeleton card-skeleton__container">
+    <Paper style={{ width: '100%' }} elevation={3}>
+      <Grid container className="card-skeleton card-skeleton__container">
         <Grid container className="card-skeleton card-skeleton__header">
           <Grid item>
             <Skeleton variant="circle" width={40} height={40} className="card-skeleton card-skeleton__avatar" />
@@ -15,12 +15,10 @@ const CardSkeleton = (props: {}) => {
             <Skeleton variant="text" className="card-skeleton card-skeleton__title" />
           </Grid>
         </Grid>
-        <Grid container justify="center">
-          <Grid item style={{ width: '82%' }}>
-            <Skeleton variant="rect" className="card-skeleton card-skeleton__body" />
-          </Grid>
+        <Grid item style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+          <Skeleton variant="rect" className="card-skeleton card-skeleton__body" />
         </Grid>
-      </div>
+      </Grid>
     </Paper>
   );
 };
