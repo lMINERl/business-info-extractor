3618bf925964226b23c00ebcd9788b74da265c40
lMINERl <seect19944@gmail.com>
2020-09-12 10:18:41 +0200

    add backdrop story

---
+++ src/stories/backdrop/backdrop.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import BackdropDefault from '../backdrop/backdropDefault';
+
+storiesOf('BackDrop', module)
+  .addDecorator((story) => (
+    <ThemeProvider theme={createMuiTheme({ palette: { type: 'dark' } })}>
+      <div key={Date.now()} style={{ display: 'flex', justifyContent: 'center', paddingTop: '100px' }}>
+        {story()}
+      </div>
+    </ThemeProvider>
+  ))
+  .add('BackDropDefault', () => <BackdropDefault />);
